65e5502df240b19af8beeef9d41ca5c58e367931
Karim Badr <93216666+karimhbadr1@users.noreply.github.com>
2021-12-13 20:29:48 +0000

    Added the rowwise code snippet
    This is some code to be able to add values of different columns for each row and test a logical case

---
+++ rowwise.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+data<-tibble(a=c(1,2,NA),b=c(NA,4,NA),c=c("wk","ih","ah"),d=c(1,9,12))
+
+data<-data %>% 
+  rowwise() %>% 
+  mutate(e=sum(c_across(c(a:b,d)),na.rm = TRUE)) %>% 
+  ungroup()
+
+data<-data %>% 
+  rowwise() %>% 
+  mutate(f=if_else(sum(c_across(a:b),na.rm = TRUE)>0,1,2)) %>% 
+  ungroup()
+
+data<-data %>% 
+  rowwise() %>% 
+  mutate(g=if_else(sum(c_across(a:b),na.rm = TRUE)>0,TRUE,FALSE)) %>% 
+  ungroup()
